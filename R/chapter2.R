c924d868d9da5c2e4e5e1b37c7a4dee3338796bd
Mauricio Vargas <mvargas@dcc.uchile.cl>
2020-11-05 15:40:02 +0000

    version 0.1

---
+++ R/chapter2.R
@@ -0,0 +1,96 @@
+#' GLM Regression With Robust Clustered Standard Errors
+#'
+#' Fits a regression with robust clustered standard errors. This uses a quasi-poisson
+#' family and returns the estimated coefficients after computing a clustered
+#' variance-covariance matrix.
+#'
+#' @param formula A formula for the model
+#' @param data A tibble or data.frame
+#' @export
+
+yotov_robust_glm <- function(formula, data) {
+  pair <- "pair_id" # linking variable
+
+  fit <- stats::glm(stats::as.formula(formula), family = stats::quasipoisson(link = "log"),
+                    data = data)
+
+  vcov_cluster <- multiwayvcov::cluster.vcov(
+    fit,
+    cluster = data[, pair],
+    df_correction = TRUE
+  )
+
+  vcov_cluster_reduced <- vcov_cluster[
+    which(!grepl("^exporter|^importer", rownames(vcov_cluster))),
+    which(!grepl("^exporter|^importer", rownames(vcov_cluster)))
+  ]
+
+  coef_test <- lmtest::coeftest(
+    fit,
+    vcov_cluster_reduced
+  )
+
+  return(coef_test)
+}
+
+#' Extract fixed effects from regression object
+#'
+#' Takes an lm/glm object and extracts the fixed effects estimated coefficients.
+#' This function was created to be used with `left_join()` and `predict()` as it
+#' pastes the effects and allows to create a column with the predicted output.
+#'
+#' @importFrom dplyr %>%
+#' @param fit A regression object
+#' @export
+
+yotov_fixed_effects <- function(fit) {
+  panel_coef <- any(grepl("^exp_year|^imp_year|^pair_id_2", names(fit$coefficients)))
+
+  if (panel_coef) {
+    exp_coef <- fit$coefficients[grepl("^exp_", names(fit$coefficients))]
+    exp_coef <- tibble::enframe(exp_coef) %>%
+      dplyr::mutate(name = gsub("exp_year", "", name)) %>%
+      dplyr::rename(exp_year = name, fe_exp_year = value)
+
+    imp_coef <- fit$coefficients[grepl("^imp_", names(fit$coefficients))]
+    imp_coef <- tibble::enframe(imp_coef) %>%
+      dplyr::mutate(name = gsub("imp_year", "", name)) %>%
+      dplyr::rename(imp_year = name, fe_imp_year = value)
+
+    pair_coef <- fit$coefficients[grepl("^pair_id_2", names(fit$coefficients))]
+    pair_coef <- tibble::enframe(pair_coef) %>%
+      dplyr::mutate(name = gsub("pair_id_2", "", name)) %>%
+      dplyr::rename(pair_id_2 = name, fe_pair_id_2 = value)
+
+    d <- fit$data[, c("exp_year", "imp_year", "pair_id_2")]
+
+    suppressMessages(
+      d <- d %>%
+        dplyr::left_join(exp_coef) %>%
+        dplyr::left_join(imp_coef) %>%
+        dplyr::left_join(pair_coef)
+    )
+  } else {
+    exp_coef <- fit$coefficients[grepl("^exporter", names(fit$coefficients))]
+    exp_coef <- tibble::enframe(exp_coef) %>%
+      dplyr::mutate(name = gsub("exporter", "", name)) %>%
+      dplyr::rename(exporter = name, fe_exporter = value)
+
+    imp_coef <- fit$coefficients[grepl("^importer", names(fit$coefficients))]
+    imp_coef <- tibble::enframe(imp_coef) %>%
+      dplyr::mutate(name = gsub("importer", "", name)) %>%
+      dplyr::rename(importer = name, fe_importer = value)
+
+    d <- fit$data[, c("exporter", "importer")]
+
+    suppressMessages(
+      d <- d %>%
+        dplyr::left_join(exp_coef) %>%
+        dplyr::left_join(imp_coef)
+    )
+  }
+
+  d <- dplyr::mutate_if(d, is.numeric, function(x) { ifelse(is.na(x), 0, x) })
+
+  return(d)
+}
---
+++ R/download.R
@@ -0,0 +1,143 @@
+globalVariables(c("term"))
+
+#' Download the Yotov database to your local computer
+#'
+#' This command downloads the Yotov trade database and populates a local
+#' database. The download is 31.4 MB, and the database uses 3 GB on disk.
+#' During import over 3.5 GB of disk space may be used temporarily.
+#'
+#' The database is stored by default under [rappdirs::user_data_dir()], or its
+#' location can be set with the environment variable `YOTOV_DB_DIR`.
+#'
+#' @param tag What release tag of data to download. Defaults to the most recent.
+#' Releases are expected to come twice per year. See all releases at
+#' <https://github.com/pachamaltese/yotover/releases>.
+#' @param destdir Where to download the compressed file.
+#' @param cleanup Whether to delete the compressed file after loading into the database.
+#' @param verbose Whether to display messages and download progress
+#'
+#' @return NULL
+#' @export
+#' @importFrom DBI dbRemoveTable dbExistsTable dbCreateTable dbExecute
+#'   dbWriteTable dbListTables
+#'
+#' @examples
+#' \donttest{
+#' \dontrun{
+#' yotov_db_download()
+#' }
+#' }
+yotov_db_download <- function(tag = NULL, destdir = tempdir(),
+                              cleanup = TRUE, verbose = interactive()) {
+  if (verbose) msg("Downloading data...\n")
+  zfile <- get_gh_release_file("pachamaltese/yotover",
+                               tag_name = tag,
+                               destdir = destdir, verbose = verbose
+  )
+  ver <- attr(zfile, "ver")
+  if (verbose) msg("\nDecompressing and building local database...\n")
+  temp_tsv <- tempfile(fileext = ".tsv")
+  destdir <- paste0(destdir, "/yotov-trade-db")
+  destdir <- gsub("\\\\", "/", destdir)
+  try(dir.create(destdir))
+  utils::unzip(zfile, overwrite = TRUE, exdir = destdir)
+
+  for (tab in dbListTables(yotov_db())) {
+    dbRemoveTable(yotov_db(), tab)
+  }
+
+  yotov_db_disconnect()
+
+  finp <- list.files(destdir, full.names = TRUE)
+
+  for (x in seq_along(finp)) {
+    tout <- gsub(".*/", "", gsub("\\.tsv", "", finp[x]))
+
+    msg(sprintf("Creating %s ...", tout))
+
+    d <- utils::read.delim(finp[x],
+                    sep = "\t",
+                    stringsAsFactors = FALSE)
+
+    dbWriteTable(
+      yotov_db(),
+      tout,
+      d,
+      overwrite = TRUE
+    )
+
+    rm(d, tout)
+
+    yotov_db_disconnect()
+  }
+
+  file.remove(finp)
+
+  invisible(dbListTables(yotov_db()))
+  yotov_db_disconnect()
+
+  update_yotov_pane()
+  yotov_pane()
+  invisible(yotov_status)
+}
+
+
+#' @importFrom httr GET stop_for_status content accept write_disk progress
+#' @importFrom purrr keep
+get_gh_release_file <- function(repo, tag_name = NULL, destdir = tempdir(),
+                                overwrite = TRUE, verbose = interactive()) {
+  releases <- GET(
+    paste0("https://api.github.com/repos/", repo, "/releases")
+  )
+  stop_for_status(releases, "finding releases")
+
+  releases <- content(releases)
+
+  if (is.null(tag_name)) {
+    release_obj <- releases[1]
+  } else {
+    release_obj <- purrr::keep(releases, function(x) x$tag_name == tag_name)
+  }
+
+  if (!length(release_obj)) stop("No release tagged \"", tag_name, "\"")
+
+  if (release_obj[[1]]$prerelease) {
+    message("This is pre-release/sample data! It has not been cleaned or validated.")
+  }
+
+  download_url <- release_obj[[1]]$assets[[1]]$url
+  filename <- basename(release_obj[[1]]$assets[[1]]$browser_download_url)
+  out_path <- normalizePath(file.path(destdir, filename), mustWork = FALSE)
+  response <- GET(
+    download_url,
+    accept("application/octet-stream"),
+    write_disk(path = out_path, overwrite = overwrite),
+    if (verbose) progress()
+  )
+  stop_for_status(response, "downloading data")
+
+  attr(out_path, "ver") <- release_obj[[1]]$tag_name
+  return(out_path)
+}
+
+#' Remove the local Yotov database
+#'
+#' Deletes all tables from the local database.
+#'
+#' @return NULL
+#' @export
+#' @importFrom DBI dbListTables dbRemoveTable
+#'
+#' @examples
+#' \donttest{
+#' \dontrun{
+#' yotov_db_delete()
+#' }
+#' }
+yotov_db_delete <- function() {
+  for (t in yotov_db_tables()) {
+    dbRemoveTable(yotov_db(), t)
+  }
+  update_yotov_pane()
+}
+
